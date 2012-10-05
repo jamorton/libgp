df37b479852132d56b5c48bb8abae120441ec671
Jon Morton <jonanin@gmail.com>
2012-10-04 23:44:19 -0500

    A few comments in mem.h

--- src/mem.h
+++ src/mem.h
@@ -4,10 +4,13 @@
 
 #include <stdlib.h>
 
+// Make struct allocation looks pretty
 #define new(obj) ((obj *)mem_alloc(sizeof(obj)))
 #define new_array(obj, count) ((obj *)mem_alloc(sizeof(obj) * (count)))
 #define delete(ptr) (mem_free(ptr))
 
+// Use custom alloc functions in case we want to keep track of memory usage
+// or other statistics
 static inline void * mem_alloc(size_t sz)
 {
 	return malloc(sz);
